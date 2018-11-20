14201330b02e1bc438e40569a581546d1dd9cbcf
youxinjie <youxinjie@hotmail.com>
2018-11-19 20:53:49 -0600

    adding POJOs, Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Data;
+
+/**
+ * Created by Xinjie on 2018/11/19 8:50 PM.
+ */
+@Data
+public class PetType {
+
+    private String name;
+}

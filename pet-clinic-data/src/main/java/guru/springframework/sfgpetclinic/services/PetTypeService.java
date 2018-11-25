ff5d52353cd25fa5e51c76a8a62600f07d7fb4a7
youxinjie <youxinjie@hotmail.com>
2018-11-25 15:23:05 -0600

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Xinjie on 2018/11/25 3:17 PM.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}

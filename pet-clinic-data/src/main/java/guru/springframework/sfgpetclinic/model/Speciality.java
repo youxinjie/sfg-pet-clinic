649a515191f1968e4a8c7df79440834ee9494b4c
youxinjie <youxinjie@hotmail.com>
2018-11-25 15:12:17 -0600

    added specialities, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Data;
+
+/**
+ * Created by Xinjie on 2018/11/25 3:09 PM.
+ */
+@Data
+public class Speciality extends BaseEntity{
+
+    private String description;
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Data;
+
+import java.util.Set;
+
 /**
  * Created by Xinjie on 2018/11/19 8:44 PM.
  */
+@Data
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
 }

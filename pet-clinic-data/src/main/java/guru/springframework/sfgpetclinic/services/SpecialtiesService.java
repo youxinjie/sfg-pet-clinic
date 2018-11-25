35a003f755e19d4f4f1c321baaee344359b0a309
youxinjie <youxinjie@hotmail.com>
2018-11-25 15:56:00 -0600

    added Specialties. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Xinjie on 2018/11/25 3:48 PM.
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
